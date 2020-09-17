e949ec3645cb8cd53ce1ae661f290aff622254f1
James Addison <james@reciperadar.com>
2020-09-17 23:28:36 +0100

    Add naive parsing

--- src/lib/parse.spec.ts
+++ src/lib/parse.spec.ts
@@ -9,16 +9,22 @@ describe('parsing', function() {
   it('starred description line', function() {
     const text = 'test-recipe-id';
     const starred: Starred = parseStarred(text);
+
+    assert.equal(starred.recipe_id, 'test-recipe-id');
   });
 
   it('meal description line', function() {
     const text = '2x example-id @ 2020-09-17';
     const meal: Meal = parseMeal(text);
+
+    assert.equal(meal.recipe_id, 'example-id');
   });
 
   it('stock description line', function() {
     const text = 'test-product-id';
     const stock: Stock = parseStock(text);
+
+    assert.equal(stock.product_id, 'test-product-id');
   });
 
 });
--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,22 +1,25 @@
 import { Starred, Meal, Stock } from '../document';
 
 export function parseStarred(line: string): Starred {
+    const groups = line.match(/(\S+)/);
     return {
-        recipe_id: null,
+        recipe_id: groups[1],
     };
 };
 
 export function parseMeal(line: string): Meal {
+    const groups = line.match(/(\d+)x\s+(\S+)\s+@\s+(\S+)/);
     return {
-        recipe_id: null,
-        datetime: null,
-        servings: null,
+        recipe_id: groups[2],
+        datetime: groups[3],
+        servings: parseInt(groups[1]),
     };
 };
 
 export function parseStock(line: string): Stock {
+    const groups = line.match(/(\S+)/);
     return {
-        product_id: null,
+        product_id: groups[1],
         magnitude: null,
         units: null,
     };
