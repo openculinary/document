f3fa1ebe71d1ddd999cce1837eda42798b6e6dd9
James Addison <james@reciperadar.com>
2020-09-22 21:20:37 +0100

    Nit: abbreviate variable name

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,6 +1,6 @@
 import { lt, SemVer } from 'semver';
 
-import * as prevDocument from 'document-prev';
+import * as prev from 'document-prev';
 
 import { Starred, Meal, Stock } from '../types';
 import { packageVersion } from '../version';
@@ -8,8 +8,8 @@ import { generateStarred, generateMeal, generateStock } from './generate';
 
 export function parseStarred(line: string, source: SemVer): Starred {
     if (lt(source, packageVersion)) {
-        const prevStarred: prevDocument.types.Starred = prevDocument.parse.parseStarred(line, source);
-        line = prevDocument.generate.generateStarred(prevStarred, packageVersion);
+        const prevStarred: prev.types.Starred = prev.parse.parseStarred(line, source);
+        line = prev.generate.generateStarred(prevStarred, packageVersion);
     }
     const groups = line.match(/(\S+)/);
     return {
