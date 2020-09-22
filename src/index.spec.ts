6bc6c834ddd3b606968a97c7b426646337113431
James Addison <james@reciperadar.com>
2020-09-22 18:21:38 +0100

    Update test description

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,7 +4,7 @@ import { packageVersion, types, parse, generate } from './index';
 
 describe('library exports', () => {
 
-  it('starred description line', () => {
+  it('render and parse simple entity', () => {
     const source: types.Starred = {recipe_id: 'example-recipe-id'};
     const line = generate.generateStarred(source, packageVersion);
     const target = parse.parseStarred(line, packageVersion);
