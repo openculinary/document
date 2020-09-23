bd850ac5bf096115537f0783ead40986b7011818
James Addison <james@reciperadar.com>
2020-09-23 16:32:55 +0100

    Prototyping: allow downlevel line generation

--- src/lib/generate.spec.ts
+++ src/lib/generate.spec.ts
@@ -3,13 +3,15 @@ import * as assert from 'assert';
 import * as curr from '../types';
 import { generateStarred, generateMeal, generateStock } from './generate';
 
+import { packageVersion } from '../version';
+
 describe('generation', () => {
 
   it('starred entity to line', () => {
     const starred: curr.Starred = {
         recipe_id: 'test-recipe-id',
     };
-    const text = generateStarred(starred, null);
+    const text = generateStarred(starred, packageVersion);
 
     assert.equal(text, 'test-recipe-id');
   });
@@ -20,7 +22,7 @@ describe('generation', () => {
         datetime: '2020-09-17',
         servings: 2,
     };
-    const text = generateMeal(meal, null);
+    const text = generateMeal(meal, packageVersion);
 
     assert.equal(text, '2x example-id @ 2020-09-17');
   });
@@ -31,7 +33,7 @@ describe('generation', () => {
         magnitude: null,
         units: null,
     };
-    const text = generateStock(stock, null);
+    const text = generateStock(stock, packageVersion);
 
     assert.equal('test-product-id', text);
   });
--- src/lib/generate.ts
+++ src/lib/generate.ts
@@ -1,9 +1,17 @@
-import { gt, SemVer } from 'semver';
+import { lt, SemVer } from 'semver';
 
+import * as prev from 'document-prev';
 import * as curr from '../types';
 
+import { packageVersion } from '../version';
+
 export function generateStarred(starred: curr.Starred, target: SemVer): string {
-    return `${starred.recipe_id}`;
+    const line = `${starred.recipe_id}`;
+    if (lt(target, packageVersion)) {
+        const prevStarred: prev.types.Starred = prev.parse.parseStarred(line, prev.packageVersion);
+        return prev.generate.generateStarred(prevStarred, target);
+    }
+    return line;
 };
 
 export function generateMeal(meal: curr.Meal, target: SemVer): string {
