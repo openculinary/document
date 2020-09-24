0ac722f8ea6f398955183a7b2f471a41dc9f081a
James Addison <james@reciperadar.com>
2020-09-24 15:47:38 +0100

    Refactor: use library index imports

--- src/impl.spec.ts
+++ src/impl.spec.ts
@@ -1,29 +1,27 @@
 import * as assert from 'assert';
 import * as semver from 'semver';
 
-import * as impl from './impl';
-
-import { packageVersion } from './version';
+import * as curr from './index';
 
 describe('generation', () => {
 
   it('starred entity to line', () => {
-    const starred = new impl.Starred('test-recipe-id', packageVersion);
-    const text = starred.emit(packageVersion);
+    const starred = new curr.impl.Starred('test-recipe-id', curr.packageVersion);
+    const text = starred.emit(curr.packageVersion);
 
     assert.equal(text, 'test-recipe-id');
   });
 
   it('meal entity to line', () => {
-    const meal = new impl.Meal('2x example-id @ 2020-09-17', packageVersion);
-    const text = meal.emit(packageVersion);
+    const meal = new curr.impl.Meal('2x example-id @ 2020-09-17', curr.packageVersion);
+    const text = meal.emit(curr.packageVersion);
 
     assert.equal(text, '2x example-id @ 2020-09-17');
   });
 
   it('stock entity to line', () => {
-    const stock = new impl.Stock('test-product-id', packageVersion);
-    const text = stock.emit(packageVersion);
+    const stock = new curr.impl.Stock('test-product-id', curr.packageVersion);
+    const text = stock.emit(curr.packageVersion);
 
     assert.equal('test-product-id', text);
   });
@@ -35,7 +33,7 @@ describe('parsing', () => {
   it('starred description line', () => {
     const text = 'test-recipe-id';
     const version = semver.parse('2020.9.23');
-    const starred = new impl.Starred(text, version);
+    const starred = new curr.impl.Starred(text, version);
 
     assert.equal(starred.recipe_id, 'test-recipe-id');
   });
@@ -43,7 +41,7 @@ describe('parsing', () => {
   it('meal description line', () => {
     const text = '2x example-id @ 2020-09-17';
     const version = semver.parse('2020.9.23');
-    const meal = new impl.Meal(text, version);
+    const meal = new curr.impl.Meal(text, version);
 
     assert.equal(meal.recipe_id, 'example-id');
   });
@@ -51,7 +49,7 @@ describe('parsing', () => {
   it('stock description line', () => {
     const text = 'test-product-id';
     const version = semver.parse('2020.9.23');
-    const stock = new impl.Stock(text, version);
+    const stock = new curr.impl.Stock(text, version);
 
     assert.equal(stock.product_id, 'test-product-id');
   });
